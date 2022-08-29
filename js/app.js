6bcd0b26a69352e0135bbddacf2c9322fb76a8eb
David Munro <defmunro@gmail.com>
2022-08-29 12:41:31 +0100

    Add ul li list via js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
-const list = document.querySelector(ul);
+let ul;
+let li;
+addEventListener('DOMContentLoaded', (event) => {
+      ul = document.getElementById("list");
+    for(let i = 1; i< 5; i++) {
+      li = document.createElement("li");
+    li.setAttribute("class", "section" +[i]); 
+      ul.append(li); 
+    }
+      
 
+});
 
