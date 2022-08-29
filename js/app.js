9683a7765c004d5ee4deaffe2bc3630331dfa49f
David Munro <defmunro@gmail.com>
2022-08-29 13:59:02 +0100

    Add nav via js - with attributes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
-let ul;
+let ul =document.querySelector("ul");
 let li;
-let section;
+let section = document.querySelectorAll("section");
+let main = document.querySelector("main")
+
+
+addEventListener('DOMContentLoaded', (event) => {
+
+for ( let i=0; i < main.children.length; i++) {
+console.log(section[i])
+
+     li = document.createElement("li");
+     let attribute = section[i].getAttribute("data-nav");
+     const liLinks = li.innerHTML = `<a href="#section${[i]}" >${attribute}</a>`
+    
+    ul.appendChild(li); 
+  }
+
+
+});
 
