1e3d1ce4c546100dda361b8333c32a02198292f3
David Munro <defmunro@gmail.com>
2022-08-31 17:34:47 +0100

    add active to top link and section

--- js/app.js
+++ js/app.js
@@ -8,7 +8,8 @@ addEventListener("DOMContentLoaded", (event) => {
   //create nav links via JS, link to section page
   for (let i = 0; i < main.children.length; i++) {
     console.log(section[i]);
-
+    section[0].classList.add("active")
+    
     li = document.createElement("li");
     let attribute = section[i].getAttribute("data-nav");
     li.setAttribute("data-nav", attribute);
@@ -18,19 +19,22 @@ addEventListener("DOMContentLoaded", (event) => {
     ul.append(li);
   }
 
-  // active class
+  // active class to top section
   let links = document.querySelectorAll("li");
+  section[0].classList.add("active")
+  links[0].classList.add("active")
 
   //add active class to section and li
   let sections = document.querySelectorAll("section");
   function makeActive() {
     for (const section of sections) {
       const box = section.getBoundingClientRect();
-
-      const VALUE = 150;
+       const VALUE = 150;
       if (box.top <= VALUE && box.bottom >= VALUE) {
         section.classList.add("active");
+        
         for (let i = 0; i < ul.children.length; i++) {
+            
           if (
             links[i].getAttribute("data-nav") !==
             section.getAttribute("data-nav")
