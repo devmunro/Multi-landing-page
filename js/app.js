4186900e5dad38fdc7538656f3e26e793d4908aa
David Munro <defmunro@gmail.com>
2022-08-31 17:16:15 +0100

    fix fixed bar overlapping on scroll

--- js/app.js
+++ js/app.js
@@ -1,20 +1,18 @@
-
-
+//global variables
 let ul = document.querySelector("ul");
 let li;
 let section = document.querySelectorAll("section");
 let main = document.querySelector("main");
 
 addEventListener("DOMContentLoaded", (event) => {
-
- //create nav links via JS, link to section page
+  //create nav links via JS, link to section page
   for (let i = 0; i < main.children.length; i++) {
     console.log(section[i]);
 
     li = document.createElement("li");
     let attribute = section[i].getAttribute("data-nav");
     li.setAttribute("data-nav", attribute);
-            
+
     li.innerHTML = `<a href="#section${[i + 1]}" >${attribute}</a>`;
 
     ul.append(li);
@@ -23,9 +21,7 @@ addEventListener("DOMContentLoaded", (event) => {
   // active class
   let links = document.querySelectorAll("li");
 
-
-
-  //add active class to section
+  //add active class to section and li
   let sections = document.querySelectorAll("section");
   function makeActive() {
     for (const section of sections) {
@@ -34,28 +30,24 @@ addEventListener("DOMContentLoaded", (event) => {
       const VALUE = 150;
       if (box.top <= VALUE && box.bottom >= VALUE) {
         section.classList.add("active");
-        for(let i =0; i < ul.children.length; i++) {
-            if (links[i].getAttribute("data-nav") !== section.getAttribute("data-nav") ){
-                links[i].classList.remove("active")
-            } else {
-                links[i].classList.add("active")
-            }
+        for (let i = 0; i < ul.children.length; i++) {
+          if (
+            links[i].getAttribute("data-nav") !==
+            section.getAttribute("data-nav")
+          ) {
+            links[i].classList.remove("active");
+          } else {
+            links[i].classList.add("active");
+          }
         }
-        }         
-        
-     else {
+      } else {
         section.classList.remove("active");
-        
       }
     }
   }
 
-
-
   document.addEventListener("scroll", function () {
     makeActive();
-    
-
   });
 
   // link scroll
